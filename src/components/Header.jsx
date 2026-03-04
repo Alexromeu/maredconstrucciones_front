68a2a549cbb93516a0737422e741ae6ecc8ed7ef
Alexromeu <alexxromeu@gmail.com>
2026-03-04 17:02:53 -0600

    logo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import  logo  from "../assets/logo.svg"
+import  logo  from "../assets/logo1.svg"
 import "../styles/header.css"
 
 export default function Header() {
