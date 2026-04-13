5aacd035241b71660ebeb52591420564e7012b14
Alexromeu <alexxromeu@gmail.com>
2026-04-12 20:19:11 -0500

    fgjjf

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -9,7 +9,6 @@ export default function MainLayout() {
     <>
       <Header />
       <Outlet />  
-
       <ContactInfo />
       <Footer />
     </>
--- src/components/main/Hero.jsx
+++ src/components/main/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return <>
         <section className="hero-container">
             <h1 className="hero-title">MARED</h1>
-            <h3 className="hero-slogan">Pioneros por el Comunismo, seremos como el Che!</h3>
+            <h3 className="hero-slogan">Lorem ipsum dolor sit amet coodi. Veritatis!</h3>
             <button className="hero-btn">learn more</button>
         </section>
     </>
